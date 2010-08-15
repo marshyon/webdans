8d120eb9b0fa2cd6e054356e573e6c5b29fd41d3
marshyon <marshyon@gmail.com>
2010-08-15 17:09:00 +0100

    added email functionality, configuration file - used to send mails out on errror, change, restart process enabled to stop / start dansguardian to effect changes, configuration file ( whitelist ) file is now moved to dansguarian 'lists' area

--- ninja/bin/dans_controller.pl
+++ ninja/bin/dans_controller.pl
@@ -7,18 +7,26 @@ use IO::Dir;
 use IO::File;
 use YAML;
 use Data::Dumper;
+use Config::Std;
+use Net::SMTP;
 
-my $pidfile           = '/var/run/dans_controller.pid';
-my $unblock_directory = '/etc/dansguardian/dans_controller/';
-my $status_file       = $unblock_directory . "status.txt";
-my $whitelist         = '/etc/dansguardian/dans_controller/whitelist.conf';
-my $start_cmd .= ' /etc/init.d/dansguardian start';
-my $stop_cmd  .= ' /etc/init.d/dansguardian stop';
-my @email_alerts = qw(marshyon@gmail.com nigel.thompson@gmail.com);
-my $smtp_host    = 'localhost';
-my $smtp_from    = 'dans_guardian_daemon@myhost.com';
-my $subject      = 'dans_gaurdian process died';
-my $debug        = 1;
+my $main_config = shift || 'main.cfg';
+print_help() unless ( -e $main_config );
+read_config $main_config => my %config;
+
+my $pidfile           = $config{'dans_controller'}{'pidfile'};
+my $unblock_directory = $config{'dans_controller'}{'unblock_directory'};
+my $status_file       = $config{'dans_controller'}{'status_file'};
+my $whitelist         = $config{'dans_controller'}{'whitelist'};
+my $start_cmd         = $config{'dans_controller'}{'start_cmd'};
+my $stop_cmd          = $config{'dans_controller'}{'stop_cmd'};
+my $email_alerts      = $config{'dans_controller'}{'email_alerts'};
+my $smtp_host         = $config{'dans_controller'}{'smtp_host'};
+my $smtp_from         = $config{'dans_controller'}{'smtp_from'};
+my $subject           = $config{'dans_controller'}{'subject'};
+my $debug             = $config{'dans_controller'}{'debug'};
+my $sleep             = $config{'dans_controller'}{'sleep'};
+
 
 if ( !$debug ) {
     die "not root user\n"   unless is_root_user();
@@ -32,24 +40,27 @@ if ( !$debug ) {
 
 while (1) {
     print "sleeping ....\n" if $debug;
-    sleep 10;
-    my ($unblocks, $emails) =
+    sleep $sleep;
+    my ( $unblocks, $emails ) =
       check_for_unblock_requests( { 'dir' => $unblock_directory } );
-    if (%{$unblocks}) {
-        print Dumper( $unblocks );
-        print Dumper( $emails );
+    if ( %{$unblocks} ) {
+        my $message = "unblocks : \n" . Dumper($unblocks);
+        $message .= "emails : \n" . Dumper($emails);
+        #my $to = 'marshyon@gmail.com';
+        foreach my $to (@{$email_alerts}) {
+            mail_report( { to => $to, subject => $subject, message => $message } );
+        }
         update_whitelist( { 'sites' => $unblocks } );
         restart_dansguardian( { 'start' => $start_cmd, 'stop' => $stop_cmd } );
     }
 
-    #check_dansguardian_process(
-    #    {
-    #        'emails'  => \@email_alerts,
-    #        'host'    => $smtp_host,
-    #        'from'    => $smtp_from,
-    #        'subject' => $subject
-    #    }
-    #);
+    check_dansguardian_process(
+        {
+            'host'    => $smtp_host,
+            'from'    => $smtp_from,
+            'subject' => $subject
+        }
+    );
 }
 
 sub check_for_unblock_requests {
@@ -57,9 +68,9 @@ sub check_for_unblock_requests {
     my $d = $param->{'dir'};
     print "checking for request files in $d\n" if $debug;
 
-    my %summary_by_req = ();
-    my %summary_by_addr= ();
-    my %dir     = ();
+    my %summary_by_req  = ();
+    my %summary_by_addr = ();
+    my %dir             = ();
     tie %dir, 'IO::Dir', $d;
     foreach ( keys %dir ) {
         next unless /\.req$/;
@@ -77,15 +88,15 @@ sub check_for_unblock_requests {
         print "name :: $req{'name'}\n"     if $debug;
         print "action :: $req{'action'}\n" if $debug;
         print "id:: $req{'id'}\n"          if $debug;
-        print "client:: $req{'client'}\n"          if $debug;
+        print "client:: $req{'client'}\n"  if $debug;
         update_status("request files located, about to process");
         my $addr = $req{'id'};
         $addr =~ s{ ^\#checkbx_ \d+ - }{}mxs;
         $addr =~ s{_}{\.}mxgs;
         $summary_by_req{ $req{'action'} }->{$addr}++;
-        $summary_by_addr{ $req{'client'} }->{ $req{'action'} }->{ $addr }++;
+        $summary_by_addr{ $req{'client'} }->{ $req{'action'} }->{$addr}++;
     }
-    return (\%summary_by_req, \%summary_by_addr);
+    return ( \%summary_by_req, \%summary_by_addr );
 }
 
 sub update_whitelist {
@@ -132,20 +143,40 @@ sub restart_dansguardian {
     print ">>DEBUG>> stopping with ..($stop)\n" if $debug;
     update_status("stopping dansguardian ...");
 
-    #system($stop);
+    system($stop);
     update_status("starting dansguardian ...");
 
-    #system($start);
+    system($start);
     update_status("dansguardian restarted");
 }
 
 sub check_dansguardian_process {
-    my ($param) = @_;
-    my $e       = $param->{emails};
-    my $h       = $param->{host};
-    my $f       = $param->{from};
-    my $s       = $param->{subject};
 
+    my ($param) = @_;
+
+    my $h = $param->{host};
+    my $f = $param->{from};
+    my $s = $param->{subject};
+
+    my $check_command = 'ps -ef |grep dansguardian | grep sbin | wc -l';
+    my $dans_procs    = `$check_command`;
+
+    chomp($dans_procs);
+
+    print "dans_procs : [$dans_procs]\n" if $debug;
+
+    if ( $dans_procs < 10 ) {
+        foreach my $to ( @{$email_alerts} ) {
+            mail_report(
+                {
+                    to      => $to,
+                    subject => 'dansguardian not running, restarting ....',
+                    message => 'restart to dans commencing'
+                }
+            );
+        }
+        system($start_cmd);
+    }
 }
 
 sub update_status {
@@ -161,4 +192,45 @@ sub update_status {
     else {
         die "can't open status file $status_file : $!\n";
     }
+}
+
+sub print_help {
+    print <<END;
+
+    $0 <configuratin file>
+
+configuration file 'main.cfg' is to be found at the base of the webdans directory
+
+either change to this directory and try to run this script again or else specify 
+it's full path 
+
+eg./
+
+    $0 /home/joe/webdans/main.cfg
+
+END
+    exit(1);
+
+}
+
+sub mail_report {
+
+    my ($param) = @_;
+    my $to      = $param->{'to'};
+    my $subject = $param->{'subject'};
+    my $message = $param->{'message'};
+    my $smtp = Net::SMTP->new($smtp_host);
+
+    $smtp->mail( $ENV{USER} );
+    $smtp->to($to);
+
+    $smtp->data();
+    $smtp->datasend("To: $to\n");
+    $smtp->datasend("From: $smtp_from\n");
+    $smtp->datasend("Subject: $subject\n");
+    $smtp->datasend("\n");
+    $smtp->datasend("$message\n");
+    $smtp->dataend();
+
+    $smtp->quit;
 }
