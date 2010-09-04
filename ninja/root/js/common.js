1b83ebcbfd8161f164264469cf3bcbed85274962
marshyon <marshyon@gmail.com>
2010-09-04 17:25:12 +0100

    code clean up and added sort for tablesorter to bring denied entries to top of table

--- ninja/root/js/common.js
+++ ninja/root/js/common.js
@@ -12,15 +12,17 @@ $(document).ready(function() {
             var numberOfRows = $('.tablesorter tr').length;
 
             $(function() {
+                var sorting = [[2,1],[3,0]]; 
                 $("table")
                         .tablesorter({widthFixed: true, widgets: ['zebra']})
+                        .trigger("sorton",[sorting])
                         .tablesorterPager({container: $("#pager")}) 
             });
 
             var height = $('.tablesorter').height() + 70;
             if(height < 395) { height = 395; }
             $('#mainContent').height(height);
-            console.log('onload height is ', height, ' and rows are at ', numberOfRows);
+            //console.log('onload height is ', height, ' and rows are at ', numberOfRows);
 
             $('.pagesize').click(function() {
                 var height = $('.tablesorter').height() + 70;
